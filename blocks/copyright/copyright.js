653f245a3e59ecbc352b8eb04040ddec51d6460c
matt kuckuck <mkuckuck@adobe.com>
2025-06-24 15:32:36 -0400

    updated copyright block

--- blocks/copyright/copyright.js
+++ blocks/copyright/copyright.js
@@ -1,12 +1,14 @@
 
 /**
  * Adds copyright symbol and year
- * @param {Element} block The footer block element
+ * @param {Element} block The copyright block element
  */
 export default async function decorate(block) {
-  // decorate footer DOM
-  block.textContent = '';
-  const copyright = document.createElement('div');
-  copyright.innerHTML = `<p>Copyright &copy; ${new Date().getFullYear()} Whale Hacks. All Rights Reserved.</p>`;
-  block.append(copyright);
+  // decorate copyright DOM
+    const year = `&copy ${new Date().getFullYear()}`;
+    const content = block.textContent.replace("{{year}}",year);
+    block.textContent = '';
+    const copyright = document.createElement('div');
+    copyright.innerHTML = `<p>${content}</p>`;
+    block.append(copyright);
 }
